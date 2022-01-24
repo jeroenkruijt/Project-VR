55273f68c5406744b5b5bc0af305f3511e93313c
mayuurx <71316624+mayuurx@users.noreply.github.com>
2022-01-24 11:05:24 +0100

    Waypoint stuffs

---
+++ Assets/Script/NPCSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    public GameObject npcPrefab;
+    public int npcToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+    IEnumerator Spawn()
+    {
+        int count = 0;
+        while (count < npcToSpawn)
+        {
+            GameObject obj = Instantiate(npcPrefab);
+            Transform child = transform.GetChild(Random.Range(0, transform.childCount - 1));
+            obj.GetComponent<WaypointNavigator>().currentWaypoint = child.GetComponent<Waypoint>();
+            obj.transform.position = child.position;
+
+            yield return new WaitForEndOfFrame();
+
+            count++;
+        }
+    }
+}
