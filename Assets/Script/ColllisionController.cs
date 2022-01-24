010fd458d72d22678b8f2d2f4c11be5d7b3ceb1e
rpeyn <74733523+rpeyn@users.noreply.github.com>
2022-01-24 15:36:23 +0100

    UI working

--- Assets/Script/ColllisionController.cs
+++ Assets/Script/ColllisionController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColllisionController : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class ColllisionController : MonoBehaviour
         if (other.collider.GetType() == typeof(BoxCollider))
         {
             Debug.Log("gameover");
-            
+            SceneManager.LoadScene("AccidentScreen");
+
         }
         if (other.collider.GetType() == typeof(SphereCollider))
         {
